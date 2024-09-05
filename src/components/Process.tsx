85ddc60e3dd63b524a1de797ed169ad03403a954
abc <abc@example.com>
2024-09-05 22:24:31 +0530

    process section done

--- src/components/Process.tsx
+++ src/components/Process.tsx
@@ -1,14 +1,15 @@
 import Image from 'next/image';
 import Heading from './ui/Heading';
 
+
 export default function Process() {
     return (
         <section className="py-12 bg-white">
             <Heading>
                 Process
             </Heading>
-            <Image src= "/easymyprocess.gif" width={2000} height={2000} alt="Process" className=" w-full h-full py-12  md:px-12 md:flex hidden bg-white" />
-            <Image src= "/easymyprocess.gif" width={2000} height={2000} alt="Process" className=" w-full h-full py-12  px-4 md:hidden bg-white" />
+            <Image src= "/main.gif" width={2000} height={2000} alt="Process" className=" w-full h-full py-12  md:px-12 md:flex hidden bg-white" />
+            <Image src= "/main.gif" width={2000} height={2000} alt="Process" className=" w-full h-full py-12  px-4 md:hidden bg-white" />
         </section>
     )
 }
